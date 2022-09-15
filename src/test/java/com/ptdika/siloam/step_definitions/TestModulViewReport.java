388def1b194bac09b641148f2105e9d431ff0f3a
Ubaidillah <ubaidillah.jobs@gmail.com>
2022-09-15 23:26:39 +0700

    add modul view report

--- src/test/java/com/ptdika/siloam/step_definitions/TestModulViewReport.java
+++ src/test/java/com/ptdika/siloam/step_definitions/TestModulViewReport.java
@@ -1,5 +1,7 @@
 package com.ptdika.siloam.step_definitions;
 
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.WebDriver;
 
 import com.ptdika.siloam.pages.ModulViewReport;
@@ -19,23 +21,88 @@ public class TestModulViewReport {
 		extentTest = Hooks.extentTest;
 	}
 	
-	@When("User Admin click view and report")
-	public void user_admin_click_view_and_report() {
-		extentTest.log(LogStatus.PASS, "User Admin click view and report");
+	@When("User on menu dashboard")
+	public void User_on_menu_dashboard() {
+		assertEquals(modul.getTxtPageModul(), "Home");
+		extentTest.log(LogStatus.PASS, "User on menu dashboard");	
+	}
+	
+	@And("User Admin view and report")
+	public void user_admin_view_and_report() {
+		modul.onFeatureViewReport();
+		extentTest.log(LogStatus.PASS, "User Admin view and report");
 	}
 	
 	@And("User Admin click start date")
 	public void user_admin_click_start_date() {
+		modul.startDate();
 		extentTest.log(LogStatus.PASS, "User Admin click start date");
 	}
 	
 	@And("User Admin click end date")
 	public void user_admin_click_end_date() {
+		modul.endDate();
 		extentTest.log(LogStatus.PASS, "User Admin click end date");
 	}
 	
-	@Then("User Admin click button filter")
+	@And("User Admin click button filter")
 	public void user_admin_click_button_filter() {
+		modul.filter();
 		extentTest.log(LogStatus.PASS, "User Admin click button filter");
 	}
+	
+	@Then("User Admin click button redo")
+	public void user_admin_click_button_redo() {
+		modul.redo();
+		extentTest.log(LogStatus.PASS, "User Admin click button redo");
+	}
+	
+//	@Then("Alert on end date credentials")
+//	public void Alert_on_end_date_credentials() {
+//		assertEquals(modul.getTxtAlertInfo(), "Please fill out this field.");
+//		extentTest.log(LogStatus.PASS, "Alert on end date credentials");
+//	}
+	
+	@Then("User click button export")
+	public void user_click_button_export() {
+		modul.export();
+		extentTest.log(LogStatus.PASS, "User click button export");
+	}
+	
+	@Then("User click button download")
+	public void user_click_button_download() {
+		modul.download();
+		extentTest.log(LogStatus.PASS, "User click button download");
+	}
+	
+	@When("User Admin click View")
+	public void user_admin_click_button_View() {
+		modul.view();
+		extentTest.log(LogStatus.PASS, "User Admin click View");
+	}
+	
+	@Then("User Admin click Preview Data")
+	public void user_admin_click_preview_data() {
+		modul.view();
+		extentTest.log(LogStatus.PASS, "User Admin click Preview Data");
+	}
+	
+	@Then("User Admin click Foto Faskes Awal")
+	public void user_admin_click_foto_faskes_awal() {
+		modul.faskesAwal();
+		extentTest.log(LogStatus.PASS, "User Admin click Foto Faskes Awal");
+	}
+	
+	@Then("User Admin click Foto Faskes Tujuan")
+	public void user_admin_click_foto_faskes_tujuan() {
+		modul.faskesTujuan();
+		extentTest.log(LogStatus.PASS, "User Admin click Foto Faskes Tujuan");
+	}
+
+	@Then("User Admin click scan PDF Agreement")
+	public void user_admin_click_scan_PDF_agreement() {
+		modul.pdfAgreement();
+		extentTest.log(LogStatus.PASS, "User Admin click scan PDF Agreement");
+	}
+	
 }
--- src/test/resources/2.ModulViewReportUbai.feature
+++ src/test/resources/2.ModulViewReportUbai.feature
@@ -2,7 +2,37 @@ Feature: Dashboard Admin
 
 @tag1
 Scenario: Dashboard User Function
-			When User Admin click view and report
+			When User on menu dashboard
+			And User Admin view and report
 			And User Admin click start date
 			And User Admin click end date
-			Then User Admin click button filter
+			And User Admin click button filter
+			Then User Admin click button redo 
+			And User Admin click start date
+			And User Admin click button filter
+#			Then Alert on end date credentials
+			Then User Admin click button redo 
+			And User Admin click end date
+			And User Admin click button filter
+			And User Admin click start date
+			And User Admin click end date
+			And User Admin click button filter
+			Then User click button export
+			Then User click button download
+
+@tag2
+Scenario: View on Data Table
+			When User Admin click View
+			Then User Admin click Preview Data
+			
+@tag3
+Scenario: View on Foto Faskes Awal
+			Then User Admin click Foto Faskes Awal
+			
+@tag4
+Scenario: View on Foto Faskes Tujuan
+			Then User Admin click Foto Faskes Tujuan
+			
+@tag5
+Scenario: View on Foto PDF Agreement
+			Then User Admin click scan PDF Agreement
