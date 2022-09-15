e22c3d816e9694ce9d991d34699048e6dd8a319b
Ubaidillah <ubaidillah.jobs@gmail.com>
2022-09-15 23:27:19 +0700

    default framework

--- src/main/java/com/ptdika/siloam/utils/TestScenarios.java
+++ src/main/java/com/ptdika/siloam/utils/TestScenarios.java
@@ -2,10 +2,9 @@ package com.ptdika.siloam.utils;
 
 public enum TestScenarios {
 	
-	T1("User invalid login"),
-	T2("User valid login"),
-	T3("Dashboard User Function"),
-	T4("Buzz Function");
+	T1("User valid login"),
+	T2("Dashboard User Function"),
+	T3("Buzz Function");
 	
 	private String testName;
 	
--- src/test/java/com/ptdika/siloam/step_definitions/TestRunnerUbai.java
+++ src/test/java/com/ptdika/siloam/step_definitions/TestRunnerUbai.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(tags="",
-				features="src/test/resources/5.LoginSalesUbai.feature",
+				features={"src/test/resources/1.LoginAdminUbai.feature","src/test/resources/2.ModulViewReportUbai.feature"},
 				glue = "com.ptdika.siloam.step_definitions",
 				plugin = {"pretty", "html:target/cucumber-reports.html","json:target/cucumber.json"})
 
