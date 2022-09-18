93d55604c489d768093a0d91ab4a28c577fce347
FissaLarasati <fissalarasati@gmail.com>
2022-09-18 12:39:49 +0700

    update login sales

--- src/main/java/com/ptdika/siloam/pages/LoginSales.java
+++ src/main/java/com/ptdika/siloam/pages/LoginSales.java
@@ -1,5 +1,55 @@
 package com.ptdika.siloam.pages;
 
-public class LoginSales {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.ptdika.siloam.drivers.DriverSingleton;
+
+public class LoginSales {
+	private WebDriver driver;
+
+	public LoginSales() {
+		this.driver = DriverSingleton.getDriver();
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//input[@placeholder='Username']")
+	private WebElement username;
+
+	@FindBy(xpath = "//input[@placeholder='Password']")
+	private WebElement password;
+
+	@FindBy(xpath = "//button[@type='submit']")
+	private WebElement btnLogin;
+
+	@FindBy(xpath = "//div[@class='alert alert-danger alert-dismissable']")
+	private WebElement msgError;
+	
+	@FindBy (xpath = "//span[@id='spanMessage']")
+	private WebElement txtEmpty;
+
+	// Scenario
+	public void login(String username, String password) {
+		this.username.sendKeys(username);// step
+		this.password.sendKeys(password);
+		btnLogin.click();
+		
+	}
+	
+	public void clickBtnLogin() {
+		btnLogin.click();
+	}
+
+	// Actual
+	public String msgInvalid() {
+		return msgError.getText();
+	}
+	
+	public String getTxtEmpty() {
+		return txtEmpty.getText();
+	}
+	
+	
 }
